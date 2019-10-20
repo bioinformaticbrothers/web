eb5a8ff8d134bbd8076e88a32a3883e45c7b042f
bioinformaticbrothers <bioinfobrothers@gmail.com>
2019-10-19 22:14:57 -0500

    fixed articles dropdown

--- articles/js/expand.js
+++ articles/js/expand.js
@@ -3,10 +3,10 @@ $(function() {
         $(this).next().slideToggle(200);
         $expand = $(this).find(">:first-child");
 
-        if($expand.text() == "+") {
-            $expand.text("-");
-        } else {
-            $expand.text("+");
-        }
+        // if($expand.text() == "+") {
+        //     $expand.text("-");
+        // } else {
+        //     $expand.text("+");
+        // }
     });
 });
