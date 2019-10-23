874f12d164a6d957dc57fca6991d63a086e1f243
mario <mariosergiovaldes145@gmail.com>
2019-10-23 18:33:03 -0500

    added function to redirect to article

--- articles/js/expand.js
+++ articles/js/expand.js
@@ -2,11 +2,20 @@ $(function() {
     $(".expand").on( "click", function() {
         $(this).next().slideToggle(200);
         $expand = $(this).find(">:first-child");
-
-        // if($expand.text() == "+") {
-        //     $expand.text("-");
-        // } else {
-        //     $expand.text("+");
-        // }
     });
+});
+function gotoarticle(page) {
+    // $(id).load('articles.html#01');
+    // $(id).next().slideToggle(200);
+    // $expand = $(this).find(">:first-child");
+    window.location.href = page;
+
+}
+
+$(function () {
+    // console.log(window.location.hash);
+    if(window.location.hash)
+        $(window.location.hash).load(window.location.hash.substring(1));
+        $(window.location.hash).next().slideToggle(200);
+        $expand = $(this).find(">:first-child");
 });
